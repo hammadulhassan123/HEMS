95ae85051951a132e4b1a1e394f84df5716890b4
Hammad Ul Hassan <108016049+hammadulhassan123@users.noreply.github.com>
2023-11-06 17:55:28 +0500

    git commit –allow-empty -m “Navbar added ”

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import NavBar from './components/navbar'
+
+function App() {
+  
+  return (
+    <>
+      <NavBar/>
+    </>
+  )
+}
+
+export default App
