4845c18de0ca2f74419e403f0aaefa0069c9cc79
Hammad Ul Hassan <108016049+hammadulhassan123@users.noreply.github.com>
2023-11-09 23:52:45 +0500

    "about-half done"

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import AboutSection from './HomeComp/aboutH'
 import Teamcard from './AboutComp/Teamcard'
+import Hero from './AboutComp/Hero'
 
 export default function About() {
   return (
     <>
+      <Hero/>
       <AboutSection/>
       <Teamcard/>
     </>
---
+++ src/components/AboutComp/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Hero = () => {
+  return (
+    <div className="heroMain d-flex min-vh-100">
+      <Container className="align-self-center text-center text-light col-md-8 offset-md-2">
+        <div className="lc-block mb-4">
+          <div editable="rich">
+            <h1 className="display-1 fw-bolder">About US</h1>
+          </div>
+        </div>
+
+        <div className="lc-block">
+          <div editable="rich">
+            <p className="lead">
+                Get to know about our team
+            </p>
+
+            <p className="lead">And their role in the project</p>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Hero;
